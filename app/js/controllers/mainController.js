a04e24c3323e1d625fcc5ff77809d016f5f9bc5d
Luis <luis19prz@gmail.com>
2015-07-07 17:54:35 -0500

    bubble chart y layout responsive

--- app/js/controllers/mainController.js
+++
@@ -1,5 +0,0 @@
-angular.module('asec').controller('mainController', function ($scope) {
-    "use strict";
-
-    $scope.message = "Yeahhh ! You're ready !";
-});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,3 +12,46 @@ angular
   .module('asecApp', [
     'ngMaterial'
 ]);
+
+
+function initBubbles(){
+    var diameter = 600;
+
+    var bubbleWidth = parseInt(d3.select('#bubble-chart').style('width'), 10);
+    var bubbleHeight = parseInt(d3.select('#bubble-chart').style('height'), 10);
+
+    var svg = d3.select('#bubble-chart').append('svg')
+        .attr('width', bubbleWidth)
+        .attr('height', bubbleHeight);
+
+    var bubble = d3.layout.pack()
+        .size([bubbleWidth, bubbleHeight])
+        .padding(3) // padding between adjacent circles
+        .value(function(d) {return d.size;}) // new data will be loaded to bubble layout
+
+    var data = {"countries_msg_vol": {
+      "CA": 170, "US": 393, "CU": 9, "BR": 89, "MX": 192,"Other": 254
+    }};
+
+    var nodes = bubble.nodes(processData(data))
+        .filter(function(d){ return !d.children });
+
+    var vis = svg.selectAll('circle').data(nodes, function(d){ return d.name });
+
+    vis.enter().append('circle')
+        .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+        .attr('r', function(d) { return d.r; })
+        .attr('class', function(d) { return d.className; });
+}
+
+function processData(data) {
+    var obj = data.countries_msg_vol;
+    var newDataSet = [];
+    for(var prop in obj) {
+        newDataSet.push({name: prop, className: prop.toLowerCase(), size: obj[prop]});
+    }
+    return {children: newDataSet};
+}
+
+
+initBubbles();
